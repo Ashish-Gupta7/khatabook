ac996444c47b11329146a04062005f7a0e2198da
Ashish-Gupta7 <ash300mr@gmail.com>
2024-05-29 17:33:17 +0530

    day 3 debug and env

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
     .connect("mongodb://127.0.0.1/27017/khatabook")
     .then(() => {
-        console.log("connected with db");
+        dbgr("connected with DB");
     })
     .catch((err) => {
-        console.log(err);
+        dbgr(err);
     });
 
 module.exports = mongoose.connection;
